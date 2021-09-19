8c401bc2066820ccebea70224b9836bc568462d7
srujan <srujan001ssa@gmail.com>
2021-09-19 13:56:28 +0530

    added style, fixed bugs

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const billAmount = document.querySelector("#bill-amount");
-const cashGiven = document.querySelector("#cash-given");
-const checkButton = document.querySelector("#check-button");
-const message = document.querySelector("#error-message");
-const noOfNotes = document.querySelector(".no-of-notes");
+var billAmount = document.querySelector("#bill-amount");
+var cashGiven = document.querySelector("#cash-given");
+var checkButton = document.querySelector("#check-button");
+var message = document.querySelector("#error-message");
+var noOfNotes = document.querySelectorAll(".no-of-notes");
 const availableNotes = [2000,500,100,20,10,5,1];
 
 checkButton.addEventListener("click", function validateBillAndCashAmount(){
   hideMessage();
   if (billAmount.value > 0){
-    if (cashGiven.value >= billAmount.value){
+    if (parseInt(billAmount.value) <= parseInt(cashGiven.value)) {
       const amountToBeReturned = cashGiven.value - billAmount.value;
       calculateChange(amountToBeReturned);
     }
     else {
-      showMessage("Should I call the Police");
+      showMessage("Fancy a chat with the cops?");
     }
   }
   else{
@@ -25,6 +25,7 @@ function calculateChange(amountToBeReturned){
   for(let i=0; i< availableNotes.length; i++){
     const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
     amountToBeReturned = amountToBeReturned % availableNotes[i];
+    console.log(numberOfNotes);
     noOfNotes[i].innerText = numberOfNotes;
   }
 }
